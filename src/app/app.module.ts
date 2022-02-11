735111e53c43af7c64acac25bda975618b843128
antonymburia <antonymburia10@gmail.com>
2022-02-12 01:54:14 +0300

    create all major components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { ReposComponent } from './repos/repos.component';
+import { SearchComponent } from './search/search.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDataComponent,
+    ReposComponent,
+    SearchComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
