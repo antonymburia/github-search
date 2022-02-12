b7036be04b27281e2fddf1b824bead05916f76d7
antonymburia <antonymburia10@gmail.com>
2022-02-12 21:55:20 +0300

    collect form data

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  search:string;
+
+@Output() searchEmitter= new EventEmitter<any>();
   constructor() { }
+emitdata(){
+  this.searchEmitter.emit(this.search);
+}
+
 
   ngOnInit(): void {
   }
