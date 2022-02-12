e6a03c09bf6e950069404cc320c3453ae2afd66e
antonymburia <antonymburia10@gmail.com>
2022-02-13 01:07:40 +0300

    create user-data class

---
+++ src/app/classes/user-data.ts
@@ -0,0 +1,16 @@
+export class UserData {
+  constructor(
+    avatar_url: any,
+    name: any,
+    company: any,
+    location: any,
+    followers: any,
+    following: any,
+    logged_at: Date,
+    email: string,
+    bio: any,
+    login: any,
+    created_at: Date,
+
+  ) { }
+}
