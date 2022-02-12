c5229d8b90cf1fa7dd274327f8befb14d65623c9
antonymburia <antonymburia10@gmail.com>
2022-02-12 04:20:09 +0300

    all navbar links working as expected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReposComponent } from './repos/repos.component';
+import { UserDataComponent } from './user-data/user-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-data', component: UserDataComponent},
+  { path: 'repo-list', component: ReposComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
